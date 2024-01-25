aecd49e4c19f9b40236d98ae60c87cdd8ada9d43
farinap5 <farinap5@protonmail.com>
2024-01-25 14:26:05 -0300

    chg: version

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	var v float32 = 1.0 // version
-	var s bool    = true  // stable
+	var v float32 = 1.01 // version
+	var s bool    = false  // stable
 	src.Start(v,s)
 }
